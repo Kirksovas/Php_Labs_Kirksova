f93c533ceced43b00580118fb262f2393e756619
Kirksovas <mettarisk23@gmail.com>
2024-02-10 09:07:22 +0300

    лабораторная

---
+++ Lab1/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>231-321 Кирксова Кристина</title>
+    <style>
+        header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 10px;
+            background-color: #f0f0f0; 
+        }
+
+        .header__img {
+            margin-left: 0;
+        }
+
+        .header__title {
+            text-align: center;
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <img class="header__img"  src="/Lab1/logo.svg" alt="Логотип">
+        <h1 class="header__titlw">Домашняя работа: Hello, World!</h1>
+    </header>
+    <main>
+
+    <?php
+    $content = "Hello, World!";
+    echo $content ;
+?>
+
+    </main>
+    <footer>
+
+    </footer>
+    
+
+</body>
+</html>
