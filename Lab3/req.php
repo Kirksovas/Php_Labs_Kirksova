ed67dce06870a28b89e9751724bb0e181638e6a1
Kirksovas <mettarisk23@gmail.com>
2024-02-16 23:32:03 +0300

    lab3

---
+++ Lab3/req.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Бэк 231-321</title>
+</head>
+<body>
+    <fieldset>
+        <?php
+            print_r(get_headers('https://github.com/Kirksovas/Php_Labs_Kirksova/tree/main/Lab2'));
+        ?>
+    </fieldset>
+    <a href="index.html">Вернуться назад</a>
+</body>
+</html>
