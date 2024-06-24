7ea486c2f304bc16763747d4b6276c8943775004
Kirksovas <mettarisk23@gmail.com>
2024-06-24 12:29:56 +0300

    comments 2.5

--- Project/src/View/View.php
+++ Project/src/View/View.php
@@ -1,13 +1,17 @@
-<?php
-
-namespace View;
-
-class View{
-    public function __construct(public $path){}
-    
-    public function renderHtml(string $templateName, $vars = [], int $code=200){
-        http_response_code($code);
-        extract($vars);
-        require($this->path.$templateName);
-    }
+<?php 
+ 
+namespace View; 
+ 
+class View{ 
+    public function __construct(public $path){} 
+     
+  
+    public function renderHtml(string $templateName, $vars = [], int $code=200){ 
+        // Установка HTTP-статуса ответа
+        http_response_code($code); 
+        // Извлечение переменных для доступа в шаблоне
+        extract($vars); 
+        // Подключение указанного HTML-шаблона
+        require($this->path.$templateName); 
+    } 
 }
--- Project/templates/articles/edit.php
+++ Project/templates/articles/edit.php
@@ -1,15 +1,23 @@
-<?php require(__DIR__.'/../header.php');?>
-<!-- Форма для обновления статьи -->
-<form action="<?=dirname($_SERVER['SCRIPT_NAME']);?>/article/update/<?=$article->getId();?>" method="POST">
-  <div class="form-group">
-    <label for="name">Name article</label>
-    <input type="text" class="form-control" id="name" name="name" value="<?=$article->getName();?>">
-  </div>
-  <div class="form-group">
-    <label for="text">Text</label>
-    <textarea name="text" id="text" class="form-control"><?=$article->getText();?></textarea>
-  </div>
-  <input type="hidden" name="authorId" value="<?=$article->getAuthorId();?>">
-  <button type="submit" class="btn btn-primary">Update</button>
-</form>
-<?php require(__DIR__.'/../footer.php');?>
+<?php require(__DIR__.'/../header.php'); ?> 
+<!-- Подключение шапки сайта-->
+
+<!--Форма для обновлении статьи-->
+<form action="<?=dirname($_SERVER['SCRIPT_NAME']);?>/article/update/<?=$article->getId();?>" method="POST"> 
+  <!--Поле ввода имени статьи-->
+  <div class="form-group"> 
+    <label for="name">Name article</label> 
+    <!--Ввод имени статьи, предварительно заполненное текущим значением-->
+    <input type="text" class="form-control" id="name" name="name" value="<?=$article->getName();?>"> 
+  </div> 
+  <!--Ввода текста статьи-->
+  <div class="form-group"> 
+    <label for="text">Text</label> 
+    <!--Ввод текста статьи, предварительно заполненный текущим значением-->
+    <textarea name="text" id="text" class="form-control"><?=$article->getText();?></textarea> 
+  </div> 
+  <!--Скрытое поле для передачи ID автора статьи-->
+  <input type="hidden" name="authorId" value="<?=$article->getAuthorId();?>"> 
+  <button type="submit" class="btn btn-primary">Update</button> 
+</form> 
+
+<?php require(__DIR__.'/../footer.php'); ?> 
